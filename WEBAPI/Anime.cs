c34fcab95480e4555d731e2c0c2e51b0dc3838e1
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-22 13:41:41 -0300

    feature: add GET by id, PUT and DELETE animes method

--- WEBAPI/Anime.cs
+++ WEBAPI/Anime.cs
@@ -1,7 +1,9 @@
-﻿public class Anime
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Anime
 {
     public int id { get; set; }
     public string name { get; set; }
-    public string genre { get; set;}
+    public string genre { get; set; }
     public int? rating { get; set; }
 }
--- WEBAPI/Controllers/AnimesController.cs
+++ WEBAPI/Controllers/AnimesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,6 +21,19 @@ public class AnimesController : ControllerBase
         return _context.Animes.ToList();
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Anime>> GetAnime(int id)
+    {
+        var anime = await _context.Animes.FindAsync(id);
+
+        if (anime == null)
+        {
+            return NotFound();
+        }
+
+        return anime;
+    }
+
     [HttpPost]
     public async Task<ActionResult<Anime>> PostAnime(Anime anime)
     {
@@ -27,5 +41,54 @@ public class AnimesController : ControllerBase
         await _context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(Get), new { id = anime.id }, anime);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAnime(int id, Anime anime)
+    {
+        if (id != anime.id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(anime).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!AnimeExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnime(int id)
+    {
+        var anime = await _context.Animes.FindAsync(id);
+        if (anime == null)
+        {
+            return NotFound();
+        }
+
+        _context.Animes.Remove(anime);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool AnimeExists(int id)
+    {
+        return _context.Animes.Any(e => e.id == id);
     }
 }
