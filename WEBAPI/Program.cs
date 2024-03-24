1d6d39c3bb47712e94ea478ef919de801abfc2ab
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-23 22:56:19 -0300

    feautre: add docker support

--- WEBAPI/Program.cs
+++ WEBAPI/Program.cs
@@ -26,16 +26,15 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+    //app.UseSwagger();
+  //  app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
-// Use CORS
-app.UseCors("AllowAnyOrigin");
+
 
 app.UseAuthentication();
 app.UseAuthorization();
